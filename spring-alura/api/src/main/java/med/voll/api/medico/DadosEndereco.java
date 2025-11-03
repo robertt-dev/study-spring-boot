9ca74f044b695b9d2b5dcb7076062a3d9e26af18
robertt-dev <roberttmehary.dev@gmail.com>
2025-11-03 13:57:59 -0300

    migrations-flyway

--- spring-alura/api/src/main/java/med/voll/api/medico/DadosEndereco.java
+++ spring-alura/api/src/main/java/med/voll/api/medico/DadosEndereco.java
@@ -1,13 +1,26 @@
 package med.voll.api.medico;
 
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 @Embeddable
 public record DadosEndereco(
-  String logradouro, 
-  String bairro, 
-  String cep, 
-  String cidade, 
+
+  @NotBlank
+  String logradouro,
+  
+  @NotBlank
+  String bairro,
+  
+  @NotBlank
+  @Pattern(regexp = "\\d{8}")
+  String cep,
+  
+  @NotBlank
+  String cidade,
+  
+  @NotBlank
   String uf,
   String complemento, 
   String numero) {
--- spring-alura/api/src/main/java/med/voll/api/medico/MedicoRepository.java
+++ spring-alura/api/src/main/java/med/voll/api/medico/MedicoRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
   
+    // <> tipo da entidade e o tipo da chave primaria
 }
